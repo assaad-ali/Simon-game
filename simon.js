292dd3787d8d235291f7dd1247f9473b5023956c
Ali-Assaad <aliassaadtr@gmail.com>
2022-05-22 18:16:56 +0300

    Add eventLisneters

--- simon.js
+++ simon.js
@@ -1,3 +1,24 @@
+/********************* Events Listeners /**********************/
+
+function addStartGameListeners(){
+
+    for(i = 0; i < boxes.length; i++){
+
+        console.log(boxes[i])
+        boxes[i].addEventListener('click', onBoxClick)
+    }
+}
+
+function addPressAnyKeyListener(){
+
+    document.addEventListener("keypress", startGame)
+}
+
+function removePressAnyKeyListener(){
+
+    document.removeEventListener("keypress", startGame)
+}
+
 // intialize game
 function initSimon(){
 
