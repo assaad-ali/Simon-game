c044534a6636f64758cb859ce82ecbebc8cfe1d9
Ali-Assaad <aliassaadtr@gmail.com>
2022-05-22 18:30:56 +0300

    Add global variables

--- simon.js
+++ simon.js
@@ -1,3 +1,14 @@
+/********************* Global Variables /**********************/
+
+var boxes = document.getElementsByClassName("box")
+var playing = false
+var clickCounter = 0
+var levelCounter = 1
+var gameMemory = []
+
+var boxIds = ["green-box","red-box","yellow-box","blue-box"]
+
+
 /********************* Events Listeners /**********************/
 
 function addStartGameListeners(){
